a5fa3907a834eb0d03a2d5baf962031ec28a27f7
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-02-22 12:06:16 +0800

    Develop regression algorithm

--- LinearRegression/LinearRegression/Form1.Designer.cs
+++ LinearRegression/LinearRegression/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Openprice = new System.Windows.Forms.TextBox();
+            this.HighPrice = new System.Windows.Forms.TextBox();
+            this.LowPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PredictedClose = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Openprice
+            // 
+            this.Openprice.Location = new System.Drawing.Point(110, 31);
+            this.Openprice.Name = "Openprice";
+            this.Openprice.Size = new System.Drawing.Size(144, 22);
+            this.Openprice.TabIndex = 1;
+            // 
+            // HighPrice
+            // 
+            this.HighPrice.Location = new System.Drawing.Point(110, 69);
+            this.HighPrice.Name = "HighPrice";
+            this.HighPrice.Size = new System.Drawing.Size(144, 22);
+            this.HighPrice.TabIndex = 2;
+            // 
+            // LowPrice
+            // 
+            this.LowPrice.Location = new System.Drawing.Point(110, 111);
+            this.LowPrice.Name = "LowPrice";
+            this.LowPrice.Size = new System.Drawing.Size(144, 22);
+            this.LowPrice.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Open Price:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Low Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Predicted Close:";
+            // 
+            // PredictedClose
+            // 
+            this.PredictedClose.AutoSize = true;
+            this.PredictedClose.Location = new System.Drawing.Point(137, 236);
+            this.PredictedClose.Name = "PredictedClose";
+            this.PredictedClose.Size = new System.Drawing.Size(0, 16);
+            this.PredictedClose.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "High Price:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(295, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PredictedClose);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LowPrice);
+            this.Controls.Add(this.HighPrice);
+            this.Controls.Add(this.Openprice);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Linear Regression";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Openprice;
+        private System.Windows.Forms.TextBox HighPrice;
+        private System.Windows.Forms.TextBox LowPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label PredictedClose;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- LinearRegression/LinearRegression/Form1.cs
+++ LinearRegression/LinearRegression/Form1.cs
@@ -1,20 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LinearRegression
 {
     public partial class Form1 : Form
     {
+        // Open File Code
+        /*string path;
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        // To list only csv files, we need to add this filter
+        openFileDialog.Filter = "|*.csv";
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                textBox1.Text = path;
+            }*/
+
+        //x1 is for open price
+        //x2 is for high price
+        //x3 is for low price
+        double[] x1 = { 289.8933, 360.3833, 286.9233, 251.72, 227, 301.2767, 272.58, 254.5, 234.05, 197.08, 118.47, 173.89, 199.985 };
+        double[] x2 = { 371.59, 384.29, 318.5, 264.21, 298.32, 314.6667, 313.8, 257.5, 237.4, 198.92, 180.68, 217.65, 208.44 };
+        double[] x3 = { 252.0133, 273.9, 206.8567, 208.6933, 216.1667, 271.81, 262.47, 198.59, 166.19, 108.24, 101.81, 169.93, 197.5 };
+
+        double[] y = { 259.2, 290.2533, 252.7533, 224.4733, 297.15, 275.61, 265.25, 227.54, 194.7, 123.18, 173.22, 208.31, 208.31 };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Calculate through Multiple Regression
+            double predicted_y = 16.53205682 + (Double.Parse(Openprice.Text) * -0.889831308) + (Double.Parse(HighPrice.Text) * 1.155089547) + (Double.Parse(LowPrice.Text) * 0.601626723);
+            double rounded = Math.Round(predicted_y, 2);
+            PredictedClose.Text = rounded.ToString();
         }
     }
 }
