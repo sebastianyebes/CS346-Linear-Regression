32343062929b9df745271248f69daff5f04c89cc
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-02-26 15:24:59 +0800

    Develop Data Table and Import feature

--- LinearRegression/LinearRegression/Form1.Designer.cs
+++ LinearRegression/LinearRegression/Form1.Designer.cs
@@ -37,11 +37,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.PredictedClose = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Graph_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 167);
+            this.button1.Location = new System.Drawing.Point(501, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 38);
             this.button1.TabIndex = 0;
@@ -51,29 +55,29 @@
             // 
             // Openprice
             // 
-            this.Openprice.Location = new System.Drawing.Point(110, 31);
+            this.Openprice.Location = new System.Drawing.Point(112, 36);
             this.Openprice.Name = "Openprice";
-            this.Openprice.Size = new System.Drawing.Size(144, 22);
+            this.Openprice.Size = new System.Drawing.Size(329, 22);
             this.Openprice.TabIndex = 1;
             // 
             // HighPrice
             // 
-            this.HighPrice.Location = new System.Drawing.Point(110, 69);
+            this.HighPrice.Location = new System.Drawing.Point(112, 74);
             this.HighPrice.Name = "HighPrice";
-            this.HighPrice.Size = new System.Drawing.Size(144, 22);
+            this.HighPrice.Size = new System.Drawing.Size(329, 22);
             this.HighPrice.TabIndex = 2;
             // 
             // LowPrice
             // 
-            this.LowPrice.Location = new System.Drawing.Point(110, 111);
+            this.LowPrice.Location = new System.Drawing.Point(112, 116);
             this.LowPrice.Name = "LowPrice";
-            this.LowPrice.Size = new System.Drawing.Size(144, 22);
+            this.LowPrice.Size = new System.Drawing.Size(329, 22);
             this.LowPrice.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Location = new System.Drawing.Point(25, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 16);
             this.label1.TabIndex = 4;
@@ -82,7 +86,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 114);
+            this.label3.Location = new System.Drawing.Point(27, 119);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 16);
             this.label3.TabIndex = 6;
@@ -91,7 +95,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 236);
+            this.label4.Location = new System.Drawing.Point(25, 179);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 16);
             this.label4.TabIndex = 7;
@@ -100,7 +104,7 @@
             // PredictedClose
             // 
             this.PredictedClose.AutoSize = true;
-            this.PredictedClose.Location = new System.Drawing.Point(137, 236);
+            this.PredictedClose.Location = new System.Drawing.Point(139, 179);
             this.PredictedClose.Name = "PredictedClose";
             this.PredictedClose.Size = new System.Drawing.Size(0, 16);
             this.PredictedClose.TabIndex = 8;
@@ -108,17 +112,51 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Location = new System.Drawing.Point(26, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "High Price:";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 38);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Import";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 222);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(578, 351);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Graph_Button
+            // 
+            this.Graph_Button.Location = new System.Drawing.Point(503, 128);
+            this.Graph_Button.Name = "Graph_Button";
+            this.Graph_Button.Size = new System.Drawing.Size(103, 39);
+            this.Graph_Button.TabIndex = 12;
+            this.Graph_Button.Text = "Graph";
+            this.Graph_Button.UseVisualStyleBackColor = true;
+            this.Graph_Button.Click += new System.EventHandler(this.Graph_Button_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 287);
+            this.ClientSize = new System.Drawing.Size(636, 600);
+            this.Controls.Add(this.Graph_Button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.PredictedClose);
             this.Controls.Add(this.label4);
@@ -130,6 +168,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Linear Regression";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,6 +185,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label PredictedClose;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Graph_Button;
     }
 }
 
--- LinearRegression/LinearRegression/Form1.cs
+++ LinearRegression/LinearRegression/Form1.cs
@@ -1,43 +1,105 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Windows.Forms;
 
 namespace LinearRegression
 {
     public partial class Form1 : Form
     {
-        // Open File Code
-        /*string path;
-        OpenFileDialog openFileDialog = new OpenFileDialog();
+        // Initialize Variables
+        string path;
 
-        // To list only csv files, we need to add this filter
-        openFileDialog.Filter = "|*.csv";
+        public Form1()
+        {
+            InitializeComponent();
+            Graph_Button.Enabled = false;  
+
+            // Initialize Grid View
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "Date";
+            dataGridView1.Columns[1].Name = "Open";
+            dataGridView1.Columns[2].Name = "High";
+            dataGridView1.Columns[3].Name = "Low";
+            dataGridView1.Columns[4].Name = "Close";
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void Fill_Data(string[] date, string[] open, string[] high, string[] low, string[] close)
+        {
+            for(int i = 0; i < date.Length; i++)
+            {
+                dataGridView1.Rows.Add(date[i], open[i], high[i], low[i], close[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Openprice.Text != "" && HighPrice.Text != "" && LowPrice.Text != "")
+            {
+                // Calculate through Multiple Regression
+                double predicted_y = 16.53205682 + (Double.Parse(Openprice.Text) * -0.889831308) + (Double.Parse(HighPrice.Text) * 1.155089547) + (Double.Parse(LowPrice.Text) * 0.601626723);
+                double rounded = Math.Round(predicted_y, 2);
+                PredictedClose.Text = rounded.ToString();
+            }
+            else
+            {
+                PredictedClose.Text = "Invalid";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] arr_Date = new string[70];
+            string[] arr_Open = new string[70];
+            string[] arr_High = new string[70];
+            string[] arr_Low = new string[70];
+            string[] arr_Close = new string[70];
+
+            // Open File Code
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // To list only csv files, we need to add this filter
+            openFileDialog.Filter = "|*.csv";
             DialogResult result = openFileDialog.ShowDialog();
 
             if (result == DialogResult.OK)
             {
                 path = openFileDialog.FileName;
-                textBox1.Text = path;
-            }*/
+            }
 
-        //x1 is for open price
-        //x2 is for high price
-        //x3 is for low price
-        double[] x1 = { 289.8933, 360.3833, 286.9233, 251.72, 227, 301.2767, 272.58, 254.5, 234.05, 197.08, 118.47, 173.89, 199.985 };
-        double[] x2 = { 371.59, 384.29, 318.5, 264.21, 298.32, 314.6667, 313.8, 257.5, 237.4, 198.92, 180.68, 217.65, 208.44 };
-        double[] x3 = { 252.0133, 273.9, 206.8567, 208.6933, 216.1667, 271.81, 262.47, 198.59, 166.19, 108.24, 101.81, 169.93, 197.5 };
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                int i = 0;
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
 
-        double[] y = { 259.2, 290.2533, 252.7533, 224.4733, 297.15, 275.61, 265.25, 227.54, 194.7, 123.18, 173.22, 208.31, 208.31 };
-        public Form1()
-        {
-            InitializeComponent();
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                while(!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+                    arr_Date[i] = fields[0];
+                    arr_Open[i] = fields[1];
+                    arr_High[i] = fields[2];
+                    arr_Low[i] = fields[3];
+                    arr_Close[i] = fields[4];
+                    i++;
+                }
+            }
+
+            Fill_Data(arr_Date, arr_Open, arr_High, arr_Low, arr_Close);
+            Graph_Button.Enabled = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Graph_Button_Click(object sender, EventArgs e)
         {
-            // Calculate through Multiple Regression
-            double predicted_y = 16.53205682 + (Double.Parse(Openprice.Text) * -0.889831308) + (Double.Parse(HighPrice.Text) * 1.155089547) + (Double.Parse(LowPrice.Text) * 0.601626723);
-            double rounded = Math.Round(predicted_y, 2);
-            PredictedClose.Text = rounded.ToString();
+            Graph_Form form = new Graph_Form();
+            form.ShowDialog();
         }
     }
 }
